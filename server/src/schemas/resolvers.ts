6cfb907318c2b9656d07bf2796805a410aed3cbe
kavue <kvue06@gmail.com>
2025-01-23 21:34:29 -0600

    update resolvers & typeDefs

---
+++ server/src/schemas/resolvers.ts
@@ -0,0 +1,68 @@
+import UserModel from '../models/User.js';
+import { signToken } from '../services/auth.js';
+
+interface User {
+    _id: string;
+    username: string;
+    email: string;
+    savedBooks?: any[];
+}
+
+interface Context {
+    user?: User;
+}
+
+interface BookInput {
+    bookId: string;
+    title: string;
+    authors: string[];
+    description?: string;
+    image?: string;
+    link?: string;
+}
+
+export const resolvers = {
+    Query: {
+        me: async (_: any, __: any, { user }: Context) => {
+            if (!user) throw new Error('You must be logged in');
+            return await UserModel.findById(user._id);
+        },
+    },
+
+    Mutation: {
+        createUser: async (_: any, { username, email, password }: { username: string, email: string, password: string }) => {
+            const user = await UserModel.create({ username, email, password });
+            const token = signToken(username, email, user._id);
+            return { token, user };
+        },
+
+        login: async (_: any, { email, username, password }: { email?: string, username?: string, password: string }) => {
+            const user = await UserModel.findOne({ $or: [{ email }, { username }] });
+            if (!user || !(await user.isCorrectPassword(password))) {
+                throw new Error('Invalid credentials');
+            }
+            const token = signToken(user.username, user.email, user._id);
+            return { token, user };
+        },
+
+        saveBook: async (_: any, { bookData }: { bookData: BookInput }, { user }: Context) => {
+            if (!user) throw new Error('You must be logged in');
+            return await UserModel.findByIdAndUpdate(
+                user._id,
+                { $addToSet: { savedBooks: bookData } },
+                { new: true, runValidators: true }
+            );
+        },
+
+        deleteBook: async (_: any, { bookId }: { bookId: string }, { user }: Context) => {
+            if (!user) throw new Error('You must be logged in');
+            return await UserModel.findByIdAndUpdate(
+                user._id,
+                { $pull: { savedBooks: { bookId } } },
+                { new: true }
+            );
+        },
+    },
+};
+
+export default resolvers;
--- server/src/schemas/typeDefs.ts
+++ server/src/schemas/typeDefs.ts
@@ -1,44 +1,44 @@
 const typeDefs = `
-type User {
+  type User {
     _id: ID!
     username: String!
     email: String!
-    savedBooks: [Book]
+    savedBooks: [Book]!  # Ensure that savedBooks is always an array
   }
-  
+
   type Book {
     bookId: String!
     title: String!
     authors: [String]
-    description: String
-    image: String
+    description: String  # Made optional
+    image: String  # Made optional
     link: String
   }
-  
+
   type Auth {
     token: String!
     user: User!
   }
-  
+
   input BookInput {
     bookId: String!
     title: String!
     authors: [String]
-    description: String
-    image: String
+    description: String  # Made optional
+    image: String  # Made optional
     link: String
   }
-  
+
   type Query {
     me: User
   }
-  
+
   type Mutation {
     createUser(username: String!, email: String!, password: String!): Auth
     login(email: String, username: String, password: String!): Auth
     saveBook(bookData: BookInput!): User
     deleteBook(bookId: String!): User
   }
-  `;
-  
+`;
+
 export default typeDefs;
