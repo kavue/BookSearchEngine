c1fb59e5eafb5dd8e6ffdf8f304d3acec056f58f
kavue <kvue06@gmail.com>
2025-01-29 01:11:03 -0600

    Update mutations file

--- client/src/utils/mutations.ts
+++ client/src/utils/mutations.ts
@@ -13,11 +13,12 @@ export const LOGIN_USER = gql`
 `;
 
 export const ADD_USER = gql`
-  mutation AddUser($email: String!, $password: String!) {
-    addUser(email: $email, password: $password) {
+  mutation AddUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
       token
       user {
         _id
+        username
         email
       }
     }
