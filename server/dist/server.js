0c7c4d131c10be23a60362f38339f8995e43e670
kavue <kvue06@gmail.com>
2025-01-29 00:43:14 -0600

    Update server file

--- server/dist/server.js
+++ server/dist/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
 import path from 'node:path';
-import { fileURLToPath } from 'node:url'; // Add this import
+import { fileURLToPath } from 'node:url';
 import { typeDefs, resolvers } from './schemas/index.js';
 import db from './config/connection.js';
 const __filename = fileURLToPath(import.meta.url);
